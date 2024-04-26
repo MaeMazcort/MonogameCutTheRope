8162c7e4496fdc49994da99546fed7e301258683
MaeMazcort <mazcortmae@gmail.com>
2024-04-25 19:00:08 -0600

    Removed Drawing library

--- CandyVpt.cs
+++ CandyVpt.cs
@@ -1,8 +1,8 @@
 ﻿// Status: Completed. Check Texture instead of Image
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,17 +14,13 @@ namespace Project1
     {
         public Texture2D Texture { get; set; }
 
-        public CandyVpt(float x, float y, int id, int level, Texture2D texture) : base(x, y, id, false, Color.DarkRed, 12f, level)
+        public CandyVpt(float x, float y, int id, int level, Texture2D texture) : base(x, y, id, false, Color.CornflowerBlue, 12f, level)
         {
             Pos = new V2(x, y);
             Level = level;
             Texture = texture;
         }
 
-        public void Update(Rectangle space)
-        {
-            base.Update(space);
-        }
 
         public void ApplyForce(V2 force)
         {
