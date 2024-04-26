c182d68469c01bd5127971be5a7cfe665cada111
MaeMazcort <mazcortmae@gmail.com>
2024-04-25 20:02:26 -0600

    Star class completed

--- Star.cs
+++ Star.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿// Status: Completed
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Project1
 {
-    internal class Star
+    public class Star
     {
+        public PointF Position { get; set; }
+        public int Points { get; set; }
+        public bool IsCollected { get; set; }
+        public int Level { get; set; }
+
+        public Texture2D Texture { get; set; }
+
+        public Star(float x, float y, int level, Texture2D texture)
+        {
+            Position = new PointF(x, y);
+            Points = 10;
+            IsCollected = false;
+            Level = level;
+            Texture = texture;
+        }
+
+        public void CheckCollision(CandyVpt candy)
+        {
+            float distance = Distance(Position, new PointF(candy.Pos.X, candy.Pos.Y));
+            if (distance < 20)  // Assuming the radius for collision detection is 20
+            {
+                IsCollected = true;
+            }
+        }
+
+        private float Distance(PointF point1, PointF point2)
+        {
+            return (float)Math.Sqrt(Math.Pow(point1.X - point2.X, 2) + Math.Pow(point1.Y - point2.Y, 2));
+        }
     }
 }
