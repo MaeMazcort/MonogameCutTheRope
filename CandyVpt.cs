fb718c6447990da27063b68a553534d4082bcac2
MaeMazcort <mazcortmae@gmail.com>
2024-04-25 18:10:27 -0600

    CandyVpt Class

--- CandyVpt.cs
+++ CandyVpt.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿// Status: Completed. Check Texture instead of Image
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Project1
 {
-    public class CandyVpt
+    public class CandyVpt : VptBase
     {
+        public Texture2D Texture { get; set; }
+
+        public CandyVpt(float x, float y, int id, int level, Texture2D texture) : base(x, y, id, false, Color.DarkRed, 12f, level)
+        {
+            Pos = new V2(x, y);
+            Level = level;
+            Texture = texture;
+        }
+
+        public void Update(Size space)
+        {
+            base.Update(space);
+        }
+
+        public void ApplyForce(V2 force)
+        {
+            if (!IsPinned)
+            {
+                // Apply force adjusted by mass (F = ma, so a = F/m)
+                V2 acceleration = force / mass;
+                vel += acceleration;
+            }
+        }
     }
 }
