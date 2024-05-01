e30d4a9587fe462476a17660a119ba5d80260ce8
MaeMazcort <mazcortmae@gmail.com>
2024-04-30 18:54:10 -0600

    Camera class

---
+++ Camera.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Camera
+    {
+        public V2 position {  get; private set; }
+
+        // Init the camera at this position
+        public Camera(V2 startPosition)
+        {
+            position = startPosition;
+        }
+
+        // Update the camera to centralize it in a point
+        public void Follow(V2 targetPos, int screenW, int screenH)
+        {
+            position = new V2(targetPos.X - screenW / 2, targetPos.Y - screenH / 2);
+        }
+
+        public void ClampToArea(int width, int height, int xW, int yH)
+        {
+            position.X = Math.Clamp(position.X, 0, width - xW);
+            position.Y = Math.Clamp(position.Y, 0, height - yH);
+        }
+    }
+}
