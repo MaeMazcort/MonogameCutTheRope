7dfb39c2338b1b3a7e3ff482aa2473712b336dda
Tofasemix <emiliano.tofasrz@udlap.mx>
2024-05-03 00:55:50 -0600

    fast

--- VptBase.cs
+++ VptBase.cs
@@ -17,7 +17,7 @@ namespace Project1
         public float mass;
         public V2 old, vel, g;
         public VptBase p1, p2;
-        public float r, d, mag, frict = 0.5f;
+        public float r, d, mag, frict = 1f;
 
         public V2 Pos { get; set; }
         public int Level { get; set; }
